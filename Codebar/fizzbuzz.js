69614f45703c704c9ccf7fab8aaea9fd0fe88e7b
Kornelia <ez.kornelia@gmail.com>
2016-05-24 17:09:07 +0100

    changed, unisized

--- Codebar/fizzbuzz.js
+++ Codebar/fizzbuzz.js
@@ -1,9 +1,9 @@
 var fizzBuzz = function() {
 	for (var x = 1; x < 101; x = x + 1) {
-		if (!(x % 15)) {
+		if (x % 15 === 0) {
 			console.log("fizzbuzz");
 		}
-		else if (!(x % 5)) {
+		else if (x % 5 === 0) {
 			console.log("fizz");
 		}
 		else if (x % 3 === 0) {
